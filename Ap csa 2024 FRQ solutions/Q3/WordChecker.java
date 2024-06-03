a684b412e0bd5604a35da0b7be6fab6d70bbe2ba
LZ12344 <linz9@nycstudents.net>
2024-06-03 11:15:36 -0400

    Update WordChecker.java

--- Ap csa 2024 FRQ solutions/Q3/WordChecker.java
+++ Ap csa 2024 FRQ solutions/Q3/WordChecker.java
@@ -1,1 +1,31 @@
+import java.util.*;
 
+public class WordChecker {
+  private ArrayList<String> wordList;
+
+  public WordChecker(ArrayList<String> words) {
+    wordList = words;
+  }
+
+  public boolean isWordChain() {
+    boolean cool=false;
+    for(int i=1; i<wordList.size(); i++){
+      if(wordList.get(i).indexOf(wordList.get(i-1))>0){
+        cool=true;
+      }else{
+        cool=false;
+      }
+    }
+    return cool;
+  }
+
+  public ArrayList<String> createList(String target) {
+    ArrayList<String> result = new ArrayList<String>();
+     for (int i=0; i<wordList.size(); i++){
+      if(wordList.get(i).indexOf(target)==0){
+        result.add(wordList.get(i).substring(target.length()));
+      }
+    }
+    return result;             
+  }
+}
